d9523500252c0b35bfb27ee4aac5d400d89c90aa
sylvio-cavalcanti <sylviocavalcanti2020@gmail.com>
2021-08-24 19:18:02 -0300

    Outputting Lists

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
     data(){
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {title: 'name of the wind', author: 'partick rothfuss'},
+                {title: 'the way of kings', author: 'brandon sanderson'},
+                {title: 'the final empire', author: 'brandon sanderson'}
+            ]
         }
     },
 
@@ -14,17 +14,6 @@ const app = Vue.createApp({
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
         },
-        handleEvent(event, word){
-            console.log(event, event.type);
-            if(word){
-                console.log (word);
-            }
-        },
-        handleMousemove(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-            console.log(this.x, this.y); // prints to the browsers console 
-        }
     }
 });
 app.mount('#app');
