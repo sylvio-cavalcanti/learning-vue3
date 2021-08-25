c8b3c3b0d8cb4be6db1ed45ebba2abde873af036
sylvio-cavalcanti <sylviocavalcanti2020@gmail.com>
2021-08-25 10:56:06 -0300

    Dynamic Classes - IsFav book example

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'https://www.google.com',
             showBooks: true,
             books: [
-                {title: 'name of the wind', author: 'partick rothfuss', img: 'assets/1.jpg'},
-                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg'},
-                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg'}
+                {title: 'name of the wind', author: 'partick rothfuss', img: 'assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true}
             ]
         }
     },
