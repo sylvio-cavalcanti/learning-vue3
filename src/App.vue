a0565d0fb5662452b313afd0023e23f5d44474b5
sylvio-cavalcanti <sylviocavalcanti2020@gmail.com>
2021-08-30 13:07:07 -0300

    Props and Conditional Classes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <Modal :header="header" :text="text" theme="sale"/>
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      title: 'My first vue app =)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja sawg for half proce!'
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    }
+  },
+  components:{
+    Modal
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  h1{
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
 </style>
